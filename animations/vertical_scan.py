bfdbebaece31150990c446768bec546f25e8622d
Jared Harvey <jared.harvey10@gmail.com>
2026-01-08 15:44:20 -0600

    changed default coords

--- animations/vertical_scan.py
+++ animations/vertical_scan.py
@@ -2,31 +2,31 @@ from utils import my_utils, color_manager
 from animations.animation import Animation
 import time
 
-def run(coords, pixels, duration = None):
-  start_time = time.time()
-  num_pixels = len(coords)
+# def run(coords, pixels, duration = None):
+#   start_time = time.time()
+#   num_pixels = len(coords)
 
-  cm = color_manager.ColorManager()
-  cm.generate_pleasant_colors()
-  cm.shuffle()
+#   cm = color_manager.ColorManager()
+#   cm.generate_pleasant_colors()
+#   cm.shuffle()
 
-  # main loop
-  while duration is None or time.time() - start_time < duration:
-    turn_on_min, turn_on_max = -70, -60
+#   # main loop
+#   while duration is None or time.time() - start_time < duration:
+#     turn_on_min, turn_on_max = -70, -60
 
-    color = cm.next_color()
+#     color = cm.next_color()
     
-    while turn_on_min < max_y:
-      pixels.fill((0,0,0))
+#     while turn_on_min < max_y:
+#       pixels.fill((0,0,0))
 
-      turn_on_min += speed
-      turn_on_max += speed
+#       turn_on_min += speed
+#       turn_on_max += speed
 
-      for i in range(num_pixels):
-        if turn_on_min <= coords[i][2] <= turn_on_max:
-          pixels[i] = color
+#       for i in range(num_pixels):
+#         if turn_on_min <= coords[i][2] <= turn_on_max:
+#           pixels[i] = color
 
-      pixels.show()
+#       pixels.show()
 
 
 class VerticalScanAnimation(Animation):
@@ -41,7 +41,7 @@ class VerticalScanAnimation(Animation):
     self.current_color = self.color_manager.next_color()
 
   def update(self, dt):
-    speed = 0.1 * dt  # units per second
+    speed = 10 * dt  # units per second
     self.turn_on_min += speed
     self.turn_on_max += speed
 
