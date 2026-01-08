37645edc5cc334af8aff6e9298aeb0543d8e4499
Jared Harvey <jared.harvey10@gmail.com>
2026-01-08 15:47:45 -0600

    fixed values in vertical scan to better work with new coordinates

--- animations/vertical_scan.py
+++ animations/vertical_scan.py
@@ -33,21 +33,21 @@ class VerticalScanAnimation(Animation):
   name = "Vertical Scan"
 
   def setup(self):
-    self.turn_on_min = -70
-    self.turn_on_max = -60
+    self.turn_on_min = 0
+    self.turn_on_max = 50
     self.color_manager = color_manager.ColorManager()
     self.color_manager.generate_pleasant_colors()
     self.color_manager.shuffle()
     self.current_color = self.color_manager.next_color()
 
   def update(self, dt):
-    speed = 10 * dt  # units per second
+    speed = 50 * dt  # units per second
     self.turn_on_min += speed
     self.turn_on_max += speed
 
     if self.turn_on_min > self.max_z:
-      self.turn_on_min = -70
-      self.turn_on_max = -60
+      self.turn_on_min = 0
+      self.turn_on_max = 50
       self.current_color = self.color_manager.next_color()
 
     for i in range(self.num_pixels):
