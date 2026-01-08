342b73429555d6a4c49da97f7f2a57fa3164b6a0
Jared Harvey <jared.harvey10@gmail.com>
2026-01-08 15:45:01 -0600

    fixed import

--- animations/__init__.py
+++ animations/__init__.py
@@ -1,22 +1,3 @@
-from .breathing_tree import run as breathing_tree
-from .enchantment_glint import run as enchantment_glint
-from .fire import run as fire
-from .intersecting_planes import run as intersecting_planes
-from .rainbow_swirl import run as rainbow_swirl
-from .random_direction_scan import run as random_direction_scan
-from .random_rgb_all import run as random_rgb_all
-from .rgb_spheres import run as rgb_spheres
-from .scrolling_text import run as scrolling_text
-from .smooth_rgb_spectrum import run as smooth_rgb_spectrum
-from .snake import run as snake
-from .sparkle import run as sparkle
-from .swirling_candy_cane import run as swirling_candy_cane
-# from .tetrahedron import run as tetrahedron
-from .time_warp import run as time_warp
-from .tree_ception import run as tree_ception
-from .vertical_scan import run as vertical_scan
-from .xmaslights_spin import run as xmaslights_spin
-
 from .vertical_scan import VerticalScanAnimation
 
 ANIMATIONS = [
