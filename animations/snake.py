4b2c1c8409f35683af58f6201be3cbd04b106295
Jared Harvey <jared.harvey10@gmail.com>
2026-01-09 08:09:14 -0600

    removed hold time from snake to make it be able to change speed

--- animations/snake.py
+++ animations/snake.py
@@ -8,23 +8,17 @@ class SnakeAnimation(Animation):
     self.color_manager = color_manager.ColorManager()
     self.color_manager.generate_pleasant_colors()
     self.color_manager.shuffle()
-    self.hold_time = 0.0005
     self.current_color = self.color_manager.next_color()
     self.current_index = 0
     self.time_accumulator = 0
 
   def update(self, dt):
-    self.time_accumulator += dt
+    # Set current pixel to color
+    self.pixels[self.current_index] = self.current_color
 
-    if self.time_accumulator >= self.hold_time:
-      self.time_accumulator = 0
+    # Move to next pixel
+    self.current_index += 1
 
-      # Set current pixel to color
-      self.pixels[self.current_index] = self.current_color
-
-      # Move to next pixel
-      self.current_index += 1
-
-      if self.current_index >= self.num_pixels:
-        self.current_index = 0
-        self.current_color = self.color_manager.next_color()
+    if self.current_index >= self.num_pixels:
+      self.current_index = 0
+      self.current_color = self.color_manager.next_color()
