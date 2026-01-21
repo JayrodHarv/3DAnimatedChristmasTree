bf44a0522d553d522c5b8842e8a5db01cfe000c5
Jared Harvey <jared.harvey10@gmail.com>
2026-01-20 19:08:41 -0600

    attempting to fix animation speed

--- scheduler.py
+++ scheduler.py
@@ -2,11 +2,14 @@ import time
 
 def run_scheduler(pixels, coords, controller, fps=30):
     last = time.time()
+    max_dt = 1.0 / fps * 2  # safety clamp
 
     while True:
         now = time.time()
         dt = now - last
         last = now
+
+        dt = min(dt, max_dt)
 
         if not controller.paused:
             AnimClass = controller.current()
@@ -14,9 +17,9 @@ def run_scheduler(pixels, coords, controller, fps=30):
             anim.setup()
 
             scaled_dt = dt * controller.speed
-            anim.update(scaled_dt)
-
             anim.time_elapsed += scaled_dt
+
+            anim.update(scaled_dt)
 
             pixels.show()
 
