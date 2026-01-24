fa448538c30733489c3e88a13016aaccf33b84b4
Jared Harvey <jared.harvey10@gmail.com>
2026-01-24 13:36:48 -0600

    stop animation if manually play animation

--- controller.py
+++ controller.py
@@ -54,6 +54,7 @@ class AnimationController:
         self.index = (self.index - 1) % len(self.animations)
 
     def play(self, name):
+        self.shuffle_mode = False # stop shuffle mode if manual play
         self.animations[self.index].reset() # reset current animation
         self.animations[self.index].clear() # clear current animation
         for i, anim in enumerate(self.animations):
